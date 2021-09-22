69cb0fd992564bb670c91f36a98907c60302c11f
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2021-09-22 11:19:50 +0200

    Remove `OncePerHttpServerFilter` as it is deprecated

--- src/main/kotlin/com/example/HttpApplicationEnterFilter.kt
+++ src/main/kotlin/com/example/HttpApplicationEnterFilter.kt
@@ -3,18 +3,18 @@ package com.example
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Filter
-import io.micronaut.http.filter.OncePerRequestHttpServerFilter
+import io.micronaut.http.filter.HttpServerFilter
 import io.micronaut.http.filter.ServerFilterChain
 import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
 
 @Filter("/trigger")
-class HttpApplicationEnterFilter(private val requestContext: RequestContext) : OncePerRequestHttpServerFilter() {
+class HttpApplicationEnterFilter(private val requestContext: RequestContext) : HttpServerFilter {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    override fun doFilterOnce(request: HttpRequest<*>, chain: ServerFilterChain): Publisher<MutableHttpResponse<*>> {
+    override fun doFilter(request: HttpRequest<*>, chain: ServerFilterChain): Publisher<MutableHttpResponse<*>> {
         val trackingId = request.headers["X-TrackingId"]
         requestContext.trackingId = trackingId
         logger.info("Application enter ($trackingId).")
